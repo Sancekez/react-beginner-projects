9b029dd4c34be6eb097146bc13c691d650c6a8d3
Nikolai <k_k_007@mail.ru>
2022-09-07 12:57:09 +0600

    feat: add invite page

--- src/App.js
+++ src/App.js
@@ -6,10 +6,29 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  let [data, setData] = React.useState([]);
+  let [isLoading, setIsLoading] = React.useState(true)
+  let [inviteSend, setInviteSend] = React.useState(false)
+  let [invites, setInvites] = React.useState([])
+
+  React.useEffect(()=>{
+    fetch('https://reqres.in/api/users').then(res => res.json()).then(data => setData(data.data)).catch(err => console.warn(err)).finally(()=>{
+      setIsLoading(false);
+    })
+  }, []);
+
+  const onClickInvite = (id) => {
+    if(invites.includes(id)) {
+      setInvites(obj => obj.filter(_id => _id !== id));
+    } else {
+      setInvites(obj => [...obj, id]);
+    }
+  }
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {!inviteSend ? <Users setInviteSend={setInviteSend} onClickInvite={onClickInvite} invites={invites} items={data} isLoading={isLoading} setIsLoading={setIsLoading}/> : <Success count={invites.length}/>}
+    
     </div>
   );
 }
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Skeleton } from './Skeleton';
 import { User } from './User';
 
-export const Users = ({ items, isLoading }) => {
+export const Users = ({ items, isLoading ,onClickInvite, invites, setInviteSend}) => {
+  let [searchValue, setSearchValue] = React.useState('');
+
+  const onChangeSearch = (e)=>{
+    setSearchValue(e.target.value)
+  }
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input value={searchValue} onChange={(e)=>onChangeSearch(e)} type="text" placeholder="Найти пользователя..." />
       </div>
       {isLoading ? (
         <div className="skeleton-list">
@@ -19,10 +24,15 @@ export const Users = ({ items, isLoading }) => {
         </div>
       ) : (
         <ul className="users-list">
-          <User />
+          {items.filter((item)=>{
+            let fullName = item.first_name + item.last_name;
+            return fullName.toLowerCase().includes(searchValue.toLowerCase()) || item.email.toLowerCase().includes(searchValue.toLowerCase())
+          }).map((item, index) => {
+            return <User isInvited={invites.includes(item.id)} onClickInvite={onClickInvite} key={index} {...item}/>
+          })}
         </ul>
       )}
-      <button className="send-invite-btn">Отправить приглашение</button>
+      {invites.length > 0 && <button onClick={() => setInviteSend(true)} className="send-invite-btn">Отправить приглашение</button>}
     </>
   );
 };
